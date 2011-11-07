95cd56f3288a7a7fd3954fb70080acdc1a0b1d18
Timothée Roldao <timothee.roldao@gmail.com>
2011-11-07 23:57:24 +0100

    Création de la structure du site
    Création de la structure du site sans les modèles

---
+++ urls.py
@@ -0,0 +1,15 @@
+import settings
+from django.conf.urls.defaults import patterns, include, url
+
+
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+                       
+                       )
+
+
+
