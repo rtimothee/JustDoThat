956e5e77c13464b8d677acf17655548488783e3b
Timothée Roldao <timothee.roldao@gmail.com>
2011-11-24 18:13:26 +0100

    Finalisation de la recherche simple

--- apps/main/views.py
+++ apps/main/views.py
@@ -3,18 +3,34 @@ from JustDoThat.apps.utilisateur.models import *
 from JustDoThat.apps.defi.models import *
 from JustDoThat.apps.main.forms import *
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 from datetime import *
 from django.shortcuts import render_to_response
 
 def recherche (request):
-    if request.method == 'POST':
-            form = Rechercheform(request.POST)
+    if request.method == 'GET':
+            form = Rechercheform(request.GET)
             if form.is_valid():
                 requete = form.cleaned_data['demande']
                 #requetes
-                utilisateurs = User.objects.filter(username__contains=requete)
-                defis = Defi.objects.filter(titre__contains=requete).filter(description__contains=requete) #AJOUTER LA VERIFICATION DU USER
+                utilisateurs = User.objects.filter(username__icontains=requete)
+                defis = Defi.objects.filter(Q(titre__icontains=requete) | Q(description__icontains=requete) | Q(createur__in = utilisateurs))
+                #rediriger vers la page de resultats
+                return render_to_response('main/resultats.html', {'defis':defis, 'utilisateurs':utilisateurs})
+    else:
+        form = Rechercheform()
+        
+    return render_to_response('main/recherche.html', {'form':form})# Create your views here.
+
+def recherche (request):
+    if request.method == 'GET':
+            form = Rechercheform(request.GET)
+            if form.is_valid():
+                requete = form.cleaned_data['demande']
+                #requetes
+                utilisateurs = User.objects.filter(username__icontains=requete)
+                defis = Defi.objects.filter(Q(titre__icontains=requete) | Q(description__icontains=requete) | Q(createur__in = utilisateurs))
                 #rediriger vers la page de resultats
                 return render_to_response('main/resultats.html', {'defis':defis, 'utilisateurs':utilisateurs})
     else:
