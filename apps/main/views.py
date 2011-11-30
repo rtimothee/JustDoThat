ca4bdafb2f5b4485ecd983c60018f7fe06958103
Timothée Roldao <timothee.roldao@gmail.com>
2011-11-30 17:44:28 +0100

    Recherche classique et avancé fonctionnel
    Il manque juste un FieldSet mais bon si il faut je spécifierais moi
    même chaque champs du formulaire dans le template afin que
    l'intégration soit simplifié.

--- apps/main/views.py
+++ apps/main/views.py
@@ -4,24 +4,12 @@ from JustDoThat.apps.defi.models import *
 from JustDoThat.apps.main.forms import *
 from django.contrib.auth.models import User
 from django.db.models import Q
+from django.template import RequestContext
+from datetime import datetime
 
 from datetime import *
 from django.shortcuts import render_to_response
 
-def recherche (request):
-    if request.method == 'GET':
-            form = Rechercheform(request.GET)
-            if form.is_valid():
-                requete = form.cleaned_data['demande']
-                #requetes
-                utilisateurs = User.objects.filter(username__icontains=requete)
-                defis = Defi.objects.filter(Q(titre__icontains=requete) | Q(description__icontains=requete) | Q(createur__in = utilisateurs))
-                #rediriger vers la page de resultats
-                return render_to_response('main/resultats.html', {'defis':defis, 'utilisateurs':utilisateurs})
-    else:
-        form = Rechercheform()
-        
-    return render_to_response('main/recherche.html', {'form':form})# Create your views here.
 
 def recherche (request):
     if request.method == 'GET':
@@ -30,10 +18,31 @@ def recherche (request):
                 requete = form.cleaned_data['demande']
                 #requetes
                 utilisateurs = User.objects.filter(username__icontains=requete)
-                defis = Defi.objects.filter(Q(titre__icontains=requete) | Q(description__icontains=requete) | Q(createur__in = utilisateurs))
+                myquery = {}
+                if form.cleaned_data['difficulte']:
+                    myquery['difficulte'] = form.cleaned_data['difficulte']
+                    #defis = defis.filter(difficulte = form.cleaned_data['difficulte'])
+                if form.cleaned_data['categorie']:
+                    myquery['categorie'] = form.cleaned_data['categorie']
+                    #defis = defis.filter(categorie__nom = form.cleaned_data['categorie'])
+                if form.cleaned_data['fin']:
+                    myquery['fin__lte'] = form.cleaned_data['fin']
+                    myquery['fin__gte'] = datetime.now
+                    #defis = defis.filter(fin = form.cleaned_data['fin'])
+                defis = Defi.objects.filter(Q(titre__icontains=requete) | Q(description__icontains=requete) | Q(createur__in = utilisateurs)).filter(**myquery)
                 #rediriger vers la page de resultats
-                return render_to_response('main/resultats.html', {'defis':defis, 'utilisateurs':utilisateurs})
+                return render_to_response('main/recherche.html', {'defis':defis, 'utilisateurs':utilisateurs, 'form':form}, context_instance=RequestContext(request))
     else:
         form = Rechercheform()
         
-    return render_to_response('main/recherche.html', {'form':form})# Create your views here.
+    return render_to_response('main/recherche.html', {'form':form}, context_instance=RequestContext(request))# Create your views here.
+
+def recherche_av (request):
+    '''TO DO !! 
+    Champs texte
+    Difficulte
+    categorie
+    duree avant la fin
+    '''
+    return render_to_response('main/recherche.html', context_instance=RequestContext(request))# Create your views here.
+
