e588d542d7d391add01c0e521632a110bbb99219
MaschaM <marlene.vieillescazes@gmail.com>
2011-12-01 14:26:10 +0100

    Correction Settings.py
    Oups j'avais supprimé le mdp de Tim

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ MANAGERS = ADMINS
         'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'IMAC3_ProjetDjango',           # Or path to database file if using sqlite3.
         'USER': 'root',                         # Not used with sqlite3.
-        'PASSWORD': '',                     # Not used with sqlite3.
+        'PASSWORD': 'root',                     # Not used with sqlite3.
         'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
     }
