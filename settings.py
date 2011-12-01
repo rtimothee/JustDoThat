4cd2c7ed924e01432563be2ee6aa10cf7f53c226
MaschaM <marlene.vieillescazes@gmail.com>
2011-12-01 14:15:50 +0100

    Commit Marlene Fichier Settings.py ma bdd

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 #Config BDD Timothee
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'IMAC3_ProjetDjango',           # Or path to database file if using sqlite3.
@@ -21,6 +21,18 @@ DATABASES = {
         'PASSWORD': '',                     # Not used with sqlite3.
         'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
+    }
+}'''
+
+#Config BDD Marlene
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'IMAC3_ProjetDjango',           # Or path to database file if using sqlite3.
+        'USER': 'root',                         # Not used with sqlite3.
+        'PASSWORD': 'root',                     # Not used with sqlite3.
+        'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock',                             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                             # Set to empty string for default. Not used with sqlite3.
     }
 }
 
