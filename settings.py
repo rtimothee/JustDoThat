496ff57c5d4278b0a5fe84e86174c2cd9d8e6835
MaschaM <marlene.vieillescazes@gmail.com>
2011-12-08 21:50:51 +0100

    Intégration avancement :
    base.html / errorLog.html / profile.html

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 #Config BDD Timothee
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'IMAC3_ProjetDjango',           # Or path to database file if using sqlite3.
@@ -22,10 +22,10 @@ DATABASES = {
         'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
     }
-}
+}'''
 
 #Config BDD Marlene
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'IMAC3_ProjetDjango',           # Or path to database file if using sqlite3.
@@ -34,7 +34,7 @@ DATABASES = {
         'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock',                             # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                             # Set to empty string for default. Not used with sqlite3.
     }
-}'''
+}
 
 #ajoute votre config BDD
 '''DATABASES = {
