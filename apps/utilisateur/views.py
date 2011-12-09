43b7390cb321e4591859dca461520d69d59acb74
Florian Lamy <florian.lamy@gmail.com>
2011-12-09 16:19:40 +0100

    commit inutile pour recup

--- apps/utilisateur/views.py
+++ apps/utilisateur/views.py
@@ -10,6 +10,8 @@ from django.views.generic.simple import direct_to_template
 from django.contrib.auth.views import login, logout
 from django.shortcuts import render_to_response
 from JustDoThat.apps.utilisateur.tools import handle_uploaded_file
+from django.db.models import Count
+
 
 
 def login_view(request):
@@ -62,6 +64,71 @@ def register_view(request):
         utilisateur_form = UtilisateurForm()
         
     return render_to_response("utilisateur/register.html", {'user_form': user_form, 'utilisateur_form': utilisateur_form,})
+
+def send_message_view(request, pseudo):
+	if request.method == 'POST':
+        #recupération des informations du formulaire
+		message_form = MessageForm(request.POST)
+        
+        #si les infos sont valides
+		if message_form.is_valid() :
+					#creation du nouveau message
+					new_message = MessagePrive(**message_form.cleaned_data)
+					#creation du nouveau message avec comme créateur l'utilisateur connecté
+					new_message.emeteur = request.user
+					destinataire = User.objects.get(username = pseudo)
+					new_message.save()
+					
+					return HttpResponseRedirect("/user/inbox/")
+	else:
+		message_form = MessageForm()
+		message_form.destinataire = User.objects.get(username = pseudo)
+        
+	return render_to_response("utilisateur/send_message.html", {'message_form': message_form,})
+
+def inbox_view(request):
+
+	messages = MessagePrive.objects.filter(destinataire = request.user).order_by("-id")
+
+	nb = MessagePrive.objects.filter(destinataire = request.user).count()
+	users = []
+	messages1 = User.objects.none()
+	if nb > 1 :
+		for m in messages :
+			user = User.objects.get(id= m.emeteur.id)
+			if user.username in users:
+				if not messages1:
+					messages1 = messages.exclude(emeteur=user.id)
+				else:
+					messages1 += messages.exclude(emeteur=user.id)
+			else:
+				users.append(user.username)
+	return render_to_response("utilisateur/inbox.html", {'messages': messages1,})
+
+def conversation_view(request, pseudo):
+	if request.method == 'POST':
+        #recupération des informations du formulaire
+		message_form = MessageForm(request.POST)
+        
+        #si les infos sont valides
+		if message_form.is_valid() :
+					#creation du nouveau message
+					new_message = MessagePrive(**message_form.cleaned_data)
+					#creation du nouveau message avec comme créateur l'utilisateur connecté
+					new_message.emeteur = request.user
+					new_message.destinataire = User.objects.get(username = pseudo)
+					new_message.save()
+					
+					return HttpResponseRedirect("/user/conversation/"+pseudo)
+	else:
+		message_form = MessageForm()
+		message_form.destinataire = User.objects.get(username = pseudo)
+		
+		messages1 = MessagePrive.objects.filter(destinataire = request.user, emeteur=User.objects.get(username = pseudo)).order_by("id")
+		messages2 = MessagePrive.objects.filter(emeteur = request.user, destinataire=User.objects.get(username = pseudo)).order_by("id")
+		messages = messages1 | messages2
+        
+	return render_to_response("utilisateur/conversation.html", {'messages': messages, 'message_form': message_form,})
 
 #----------------------- SUPPRESSION COMPTE --------------------
 def delete_account (request, pseudo):
